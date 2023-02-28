555a4bbafe1048262ed100973d4cc41c324313d6
migue <daw2221@CALASANZSALAMANCA.local>
2023-02-28 10:11:44 +0100

    array

---
+++ javaScript/javascrip/ejercicio6.js
@@ -0,0 +1,20 @@
+function ejercicio6(){
+    alert("Trabajos arrays");
+    var variables = []; 
+    let numero = prompt("Introduce un numero");
+    console.log(nombre);
+    let nombre = prompt("Introduce un nombre de persona");
+    console.log(nombre);
+    let dato = confirm("va a añadir algún número");
+    if(dato>=0){
+        console.log(nombre)("La posición del array es " +numero);
+    }else{
+        let dato = confirm("¿Es su nombre " + nombre);
+        if (confirmacion) {
+            console.log("El nombre es correcto");
+            alert("Bienvenido")
+        }else{
+            console.log("No se sabe su nombre");
+        }
+    }
+}
