1c707ef624a1a726a47e64d484d27790e793a7eb
migue <daw2221@CALASANZSALAMANCA.local>
2023-03-08 14:16:28 +0100

    script

---
+++ javaScript/javascrip/ejercicio11.js
@@ -0,0 +1,10 @@
+function numero(){
+    let digito = Number(prompt("Introduzca un número"));
+    if(digito > 0){
+     alert("El número es positivo");
+    }else if (digito < 0){
+        alert("El número es negativo");
+    }else {
+        alert("El número es nulo");
+    }
+    }
